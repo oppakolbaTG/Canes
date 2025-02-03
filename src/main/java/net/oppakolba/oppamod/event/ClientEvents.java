2b5b6a8c74b1b8b2c0b8129566023089e319e376
mcmod <8103364@gmail.com>
2025-02-03 22:32:06 +0300

    Translate into Ru and fix problems add sounds of summon items

--- src/main/java/net/oppakolba/oppamod/event/ClientEvents.java
+++ src/main/java/net/oppakolba/oppamod/event/ClientEvents.java
@@ -1,13 +1,30 @@
 package net.oppakolba.oppamod.event;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.oppakolba.oppamod.Oppamod;
+import net.oppakolba.oppamod.util.KeyBinding;
 
 public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = Oppamod.MOD_ID, value = Dist.CLIENT)
     public static class ClientForgeEvents {
-        public static void onKeyRegister(RegisterKeyMappingsEvent event){
-
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+            if (KeyBinding.MANA_USING_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed a Key!"));
+            }
         }
-
-        public static void onKeyInput
+    }
+    @Mod.EventBusSubscriber(modid = Oppamod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientMobBusEvents{
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event){
+            event.register(KeyBinding.MANA_USING_KEY);
+        }
     }
 }
